cbaa9a595942e84d5c7ea04d66fb81ee4f059890
Joy_Zhou <zyfzjsc988@outlook.com>
2018-06-26 23:46:05 +0100

    FLB finish R

--- f_draw_FLB_by_R.R
+++ f_draw_FLB_by_R.R
@@ -1,4 +1,165 @@
 window_width = 0.05
+par(mfrow=c(2,3),cex=0.8,family='sans',mar=c(2,2,1,1),oma = c(2, 2, 0, 2))
+
+# baseball
+market_type = 'baseball'
+wd <- format(window_width, digits=2)
+dir_str <- paste('../OneDrive/DataScience_Homework/DISERTATION/datasets/FLB/',
+                 market_type,
+                 '_',
+                 wd,
+                 '_.csv',
+                 sep = "", collapse = NULL)
+flb<-read.csv(dir_str,sep=',',header=TRUE)
+loessMod25 <- loess(perc_winner ~ price, data=flb, span=0.25) # 25% smoothing span
+# get smoothed output
+smoothed25 <- predict(loessMod25) 
+# Plot it
+plot(flb$perc_winner, 
+     x=flb$price,
+     type="n", pch=20,
+     xlab="",
+     ylab="",
+     # xlab="Price", 
+     # ylab="Percentage of winners",
+     width=1,height=1,
+     xlim=c(0, 1), ylim=c(0, 1))
+title("BASEBALL",line = -1.15,adj = 0.1)
+lines(smoothed25, 
+      x=flb$price,lwd=2)
+d<-(0:1)
+lines(d,x=d,lty=2)
+
+# legend("topleft", inset=.05,lwd=c(NA,2,1),lty = c(-1, 1, 2), pch = c(4, NA, NA),
+#        c("obverses","smoothed25","diagonal"))
+
+
+
+# Soccer
+market_type = 'soccer'
+wd <- format(window_width, digits=2)
+dir_str <- paste('../OneDrive/DataScience_Homework/DISERTATION/datasets/FLB/',
+                 market_type,
+                 '_',
+                 wd,
+                 '_.csv',
+                 sep = "", collapse = NULL)
+flb<-read.csv(dir_str,sep=',',header=TRUE)
+loessMod25 <- loess(perc_winner ~ price, data=flb, span=0.25) # 25% smoothing span
+# get smoothed output
+smoothed25 <- predict(loessMod25) 
+# Plot it
+plot(flb$perc_winner, 
+     x=flb$price,
+     type="n", pch=20,
+     xlab="",
+     ylab="",
+     # xlab="Price", 
+     # ylab="Percentage of winners",
+     width=1,height=1,
+     xlim=c(0, 1), ylim=c(0, 1))
+title("SOCCER",line = -1.15,adj = 0.1)
+lines(smoothed25, 
+      x=flb$price,lwd=2)
+d<-(0:1)
+lines(d,x=d,lty=2)
+
+
+# iceball
+market_type = 'iceball'
+wd <- format(window_width, digits=2)
+dir_str <- paste('../OneDrive/DataScience_Homework/DISERTATION/datasets/FLB/',
+                 market_type,
+                 '_',
+                 wd,
+                 '_.csv',
+                 sep = "", collapse = NULL)
+flb<-read.csv(dir_str,sep=',',header=TRUE)
+loessMod25 <- loess(perc_winner ~ price, data=flb, span=0.25) # 25% smoothing span
+# get smoothed output
+smoothed25 <- predict(loessMod25) 
+# Plot it
+plot(flb$perc_winner, 
+     x=flb$price,
+     type="n", pch=20,
+     xlab="",
+     ylab="",
+     # xlab="Price", 
+     # ylab="Percentage of winners",
+     width=1,height=1,
+     xlim=c(0, 1), ylim=c(0, 1))
+title("ICEBALL",line = -1.15,adj = 0.1)
+lines(smoothed25, 
+      x=flb$price,lwd=2)
+d<-(0:1)
+lines(d,x=d,lty=2)
+
+
+
+# soccer_home
+market_type = 'soccer_home'
+wd <- format(window_width, digits=2)
+dir_str <- paste('../OneDrive/DataScience_Homework/DISERTATION/datasets/FLB/',
+                 market_type,
+                 '_',
+                 wd,
+                 '_.csv',
+                 sep = "", collapse = NULL)
+flb<-read.csv(dir_str,sep=',',header=TRUE)
+loessMod25 <- loess(perc_winner ~ price, data=flb, span=0.25) # 25% smoothing span
+# get smoothed output
+smoothed25 <- predict(loessMod25) 
+# Plot it
+plot(flb$perc_winner, 
+     x=flb$price,
+     type="n", pch=20,
+     xlab="",
+     ylab="",
+     # xlab="Price", 
+     # ylab="Percentage of winners",
+     width=1,height=1,
+     xlim=c(0, 1), ylim=c(0, 1))
+title("SOCCER(HOME)",line = -1.15,adj = 0.1)
+lines(smoothed25, 
+      x=flb$price,lwd=2)
+d<-(0:1)
+lines(d,x=d,lty=2)
+
+
+
+# soccer_away
+market_type = 'soccer_away'
+wd <- format(window_width, digits=2)
+dir_str <- paste('../OneDrive/DataScience_Homework/DISERTATION/datasets/FLB/',
+                 market_type,
+                 '_',
+                 wd,
+                 '_.csv',
+                 sep = "", collapse = NULL)
+flb<-read.csv(dir_str,sep=',',header=TRUE)
+loessMod25 <- loess(perc_winner ~ price, data=flb, span=0.25) # 25% smoothing span
+# get smoothed output
+smoothed25 <- predict(loessMod25) 
+# Plot it
+plot(flb$perc_winner, 
+     x=flb$price,
+     type="n", pch=20,
+     xlab="",
+     ylab="",
+     # xlab="Price", 
+     # ylab="Percentage of winners",
+     width=1,height=1,
+     xlim=c(0, 1), ylim=c(0, 1))
+title("SOCCER(AWAY)",line = -1.15,adj = 0.1)
+lines(smoothed25, 
+      x=flb$price,lwd=2)
+d<-(0:1)
+lines(d,x=d,lty=2)
+
+
+
+
+# soccer_draw
 market_type = 'soccer_draw'
 wd <- format(window_width, digits=2)
 dir_str <- paste('../OneDrive/DataScience_Homework/DISERTATION/datasets/FLB/',
@@ -11,20 +172,21 @@ flb<-read.csv(dir_str,sep=',',header=TRUE)
 loessMod25 <- loess(perc_winner ~ price, data=flb, span=0.25) # 25% smoothing span
 # get smoothed output
 smoothed25 <- predict(loessMod25) 
-
-
 # Plot it
 plot(flb$perc_winner, 
      x=flb$price,
-     type="p", pch=4,
-     main="FLB Loess Smoothing and Prediction", 
-     xlab="Price", 
-     ylab="Percentage of winners",
+     type="n", pch=20,
+     xlab="",
+     ylab="",
+     # xlab="Price", 
+     # ylab="Percentage of winners",
+     width=1,height=1,
      xlim=c(0, 1), ylim=c(0, 1))
+title("SOCCER(DRAW)",line = -1.15,adj = 0.1)
 lines(smoothed25, 
       x=flb$price,lwd=2)
 d<-(0:1)
 lines(d,x=d,lty=2)
 
-  legend("topleft", inset=.05,lwd=c(NA,2,1),lty = c(-1, 1, 2), pch = c(4, NA, NA),
-       c("obverses","smoothed25","diagonal"))
+mtext('Odds Price', side = 1, outer = TRUE, line = 0.5,cex=1.3)
+mtext('Percentage of Winners', side = 2, outer = TRUE, line = 0.5,cex=1.3)
